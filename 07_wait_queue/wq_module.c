72b3a12ac86826cdb955ab744ddd448b0bdc9f0f
mshafeeqkn <mshafeeqkn@gmail.com>
2024-07-06 09:36:54 +0530

    Added code for wait queue demostration

--- 07_wait_queue/wq_module.c
+++ 07_wait_queue/wq_module.c
@@ -9,16 +9,15 @@
 #include <linux/uaccess.h>
 #include <linux/kthread.h>
 #include <linux/wait.h>
-#include <linux/poll.h>
 #include <linux/sysfs.h>
 #include <linux/kobject.h>
 #include <linux/err.h>
+#include <linux/delay.h>
 
 #define FIRST_MINOR             0
 #define NUM_MINOR               1
-#define MAX_BUF_LEN             128
-#define DEV_DRIVER_NAME         "poll_dev"
-#define DEV_DRIVER_CLASS        "poll_class"
+#define DEV_DRIVER_NAME         "wq_dev"
+#define DEV_DRIVER_CLASS        "wq_class"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Robert W. Oliver II");
@@ -27,100 +26,108 @@ MODULE_VERSION("0.01");
 
 typedef unsigned int uint;
 
-static int  poll_open(struct inode *inode, struct file *file);
-static int  poll_release(struct inode *inode, struct file *file);
-static uint poll_poll(struct file *filp, struct poll_table_struct *wait);
-static ssize_t poll_read(struct file *f, char __user *buf,
-                                    size_t len, loff_t *off);
-static ssize_t poll_write(struct file *f, const char __user *buf,
+static int  wq_open(struct inode *inode, struct file *file);
+static int  wq_release(struct inode *inode, struct file *file);
+static ssize_t wq_write(struct file *f, const char __user *buf,
                                     size_t len, loff_t *off);
 
 typedef enum {
     CL_CDEV_ADD,
     CL_CLASS_CREATE,
     CL_DEVICE_CREATE,
-    CL_COMPLETE
+    CL_COMPLETE,
+    CL_THREAD,
 } cleanup_point_t;
 
 static struct file_operations fops = {
     .owner   = THIS_MODULE,
-    .open    = poll_open,
-    .release = poll_release,
-    .poll    = poll_poll,
-    .write   = poll_write,
-    .read    = poll_read
+    .open    = wq_open,
+    .release = wq_release,
+    .write   = wq_write,
 };
 
 static dev_t             dev         = 0;
-static struct cdev       poll_cdev;
+static struct cdev       wq_cdev;
 static struct class     *dev_class;
 static struct device    *dev_device;
-static char              buffer[MAX_BUF_LEN];
-static int               read_pending = false;
-#if 0
-static int               write_ready = false;
-#endif
+static int               thread_var  = 0;
 
-DECLARE_WAIT_QUEUE_HEAD(wait_queue);
+// Static declaration of wait queue
+DECLARE_WAIT_QUEUE_HEAD(wait_queue_10);
 
-static int  poll_open(struct inode *inode, struct file *file)
+// Dynamic declaration of wait queue
+static wait_queue_head_t wait_queue_20;
+
+
+static int  wq_open(struct inode *inode, struct file *file)
 {
     pr_info("Device File Opened...!!!\n");
     return 0;
 }
 
-static int  poll_release(struct inode *inode, struct file *file)
+static int  wq_release(struct inode *inode, struct file *file)
 {
     pr_info("Device File Closed...!!!\n");
     return 0;
 }
 
-static uint poll_poll(struct file *filp, struct poll_table_struct *wait)
-{
-    __poll_t mask = 0;
+static int thread_function(void *arg) {
+    uint8_t data = *(uint8_t *)arg;
+    pr_info("Creating thread: %d\n", data);
 
-    poll_wait(filp, &wait_queue, wait);
-    pr_info("POLL function called\n");
+    if(data == 1) {
+        wait_event(wait_queue_10, thread_var == 10);
+    } else if(data == 2) {
+        wait_event(wait_queue_20, thread_var == 20);
+    }
 
-    if(read_pending) {
-        pr_info("Set read poll flag in kernel to FALSE\n");
-        read_pending = false;
-        mask |= (POLLIN | POLLRDNORM);
-    }
-#if 0
-    if(write_ready) {
-        write_ready = false;
-        mask |= (POLLOUT | POLLWRNORM);
-    }
-#endif
-    return mask;
+    pr_info("thread_function: %d execution finished\n", data);
+    return 0;
 }
 
-static ssize_t poll_read(struct file *f, char __user *buf,
-                                    size_t len, loff_t *off)
-{
-    size_t data_len = strlen(buffer);
-    int pending = copy_to_user(buf, buffer, data_len);
-    pr_info("calling read len: %lu, offset: %llu\n", len, *off);
-    memset(buffer, 0, sizeof(buffer));
-    return data_len - pending;
+static int start_kernel_threads(void) {
+    struct task_struct *kthread_1, *kthread_2;
+    uint8_t t1 = 1, t2 = 2;
+
+    kthread_1 = kthread_run(thread_function, &t1, "thread_num_1");
+    if(kthread_1 == NULL) {
+        pr_err("Thread 1 create failed\n");
+        return -1;
+    }
+
+    kthread_2 = kthread_run(thread_function, &t2, "thread_num_2");
+    if(kthread_2 == NULL) {
+        pr_err("Thread 2 create failed\n");
+        thread_var = 10;
+        wake_up(&wait_queue_10);
+        return -1;
+    }
+
+    msleep(100);
+    return 0;
 }
 
-static ssize_t poll_write(struct file *f, const char __user *buf,
+static ssize_t wq_write(struct file *f, const char __user *buf,
                                     size_t len, loff_t *off)
 {
-    int pending;
-    if(len >= MAX_BUF_LEN) return 0;
-    pending = copy_from_user(buffer, buf, len);
-    *off += (len - pending);
-    read_pending = true;
-    wake_up(&wait_queue);
-    return (len - pending);
+    thread_var = simple_strtol(buf, NULL, 10);
+
+    wake_up(&wait_queue_10);
+    wake_up(&wait_queue_20);
+
+    return len;
 }
 
 static void cleanup_driver(cleanup_point_t cl)
 {
     switch(cl) {
+        case CL_THREAD:
+            thread_var = 10;
+            wake_up(&wait_queue_10);
+            thread_var = 20;
+            wake_up(&wait_queue_20);
+
+        __attribute__((__fallthrough__));
         case CL_COMPLETE:
             device_destroy(dev_class, dev);
 
@@ -130,7 +137,7 @@ static void cleanup_driver(cleanup_point_t cl)
 
         __attribute__((__fallthrough__));
         case CL_CLASS_CREATE:
-            cdev_del(&poll_cdev);
+            cdev_del(&wq_cdev);
 
         __attribute__((__fallthrough__));
         case CL_CDEV_ADD:
@@ -140,7 +147,7 @@ static void cleanup_driver(cleanup_point_t cl)
 
 static int init_driver(void)
 {
-    if(alloc_chrdev_region(&dev, FIRST_MINOR, NUM_MINOR, "poll_dev") < 0) {
+    if(alloc_chrdev_region(&dev, FIRST_MINOR, NUM_MINOR, "wq_dev") < 0) {
         pr_err("Allocating major number failed\n");
         return -1;
     }
@@ -148,9 +155,9 @@ static int init_driver(void)
     pr_info("Device number allocated - Major: %d; Minor: %d\n",
             MAJOR(dev), MINOR(dev));
 
-    cdev_init(&poll_cdev, &fops);
-    poll_cdev.owner = THIS_MODULE;
-    if((cdev_add(&poll_cdev, dev, NUM_MINOR)) < 0) {
+    cdev_init(&wq_cdev, &fops);
+    wq_cdev.owner = THIS_MODULE;
+    if((cdev_add(&wq_cdev, dev, NUM_MINOR)) < 0) {
         pr_err("Failed to add the device\n");
         cleanup_driver(CL_CDEV_ADD);
         return -1;
@@ -170,19 +177,27 @@ static int init_driver(void)
         return -1;
     }
 
+    init_waitqueue_head(&wait_queue_20);
+    if(-1 == start_kernel_threads()) {
+        cleanup_driver(CL_COMPLETE);
+        return -1;
+    }
+
     return 0;
 }
 
 static int __init hello_start(void)
 {
-    init_driver();
-    printk(KERN_INFO "Hello world\n");
-    return 0;
+    int ret = init_driver();
+    if(ret == 0) {
+        printk(KERN_INFO "wq_module Loaded\n");
+    }
+    return ret;
 }
 
 static void __exit hello_end(void)
 {
-    cleanup_driver(CL_COMPLETE);
+    cleanup_driver(CL_THREAD);
     printk(KERN_INFO "Goodbye Mr.\n");
 }
 
