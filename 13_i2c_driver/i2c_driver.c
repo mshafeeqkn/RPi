724356015ca1eb21c76dbfa9a969e3842005f6d1
Mohammed Shafeeque <mshafeeqkn@gmail.com>
2024-07-27 10:56:48 +0530

    Basic structure for the driver

--- 13_i2c_driver/i2c_driver.c
+++ 13_i2c_driver/i2c_driver.c
@@ -1,62 +1,104 @@
 #include <linux/module.h>
-#include <linux/init.h>
-#include <linux/slab.h>
-#include <linux/i2c.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/uaccess.h>
+
+#define DEVICE_NAME     "stm32f103_dev"
+#define CLASS_NAME      "stm32f103_class"
+#define STATIC_MAJOR    64
+#define MESSAGE_LEN     24
+
+static int              major_num;
+static dev_t            dev_file;
+
+static struct class    *char_class = NULL;
+static struct device   *char_device = NULL;
+
+static uint8_t stm_data[2] = {0};
+
+#define     STM_OFF_TIME        _IOW('i', 0, uint8_t)
+#define     STM_ON_TIME         _IOW('i', 1, uint8_t)
+#define     STM_START_BLINK     _IO('i', 2)
+
+#define     ON_INDEX            0
+#define     OFF_INDEX           1
 
 
-#define     I2C_BUS_NUMBER              1
-#define     STM_DEVICE_NAME             "STM32_DEV"
-#define     STM_DEVICE_ADDR             0x12
-
-static struct i2c_adapter        *stm_adapter = NULL;
-static struct i2c_client         *stm_client  = NULL;
-
-static struct i2c_board_info  stm_board_info = {
-    I2C_BOARD_INFO(STM_DEVICE_NAME, STM_DEVICE_ADDR)
-};
-
-static struct i2c_driver stm_driver = {
-    .driver = {
-        .name  = STM_DEVICE_NAME,
-        .owner = THIS_MODULE,
-    },
-};
-
-static int  __init stm_driver_init(void) {
-    int ret = -1;
-
-    stm_adapter = i2c_get_adapter(I2C_BUS_NUMBER);
-    if(stm_adapter == NULL) {
-        pr_err("Failed to get the I2C bus\n");
-        return ret;
+static long int stm_ioctl(struct file *f, unsigned int cmd,  long unsigned int arg) {
+    switch(cmd) {
+        case STM_OFF_TIME:
+            if (copy_from_user(&stm_data[ON_INDEX], (uint8_t __user *)arg, 1)) {
+			    return -EFAULT;
+            }
+            break;
+        case STM_ON_TIME:
+            if (copy_from_user(&stm_data[OFF_INDEX], (uint8_t __user *)arg, 1)) {
+			    return -EFAULT;
+            }
+            break;
+        case STM_START_BLINK:
+            break;
+        default:
+            return copy_to_user((uint8_t __user*)arg, stm_data, 2);
     }
-
-    stm_client = i2c_new_client_device(stm_adapter, &stm_board_info);
-    if(stm_client == NULL) {
-        pr_err("Unable to create i2c device\n");
-        return ret;
-    }
-
-    i2c_add_driver(&stm_driver);
-    ret = 0;
-
-    i2c_put_adapter(stm_adapter);
-    pr_info("STM32 Driver added\n");
-    return ret;
+    return 0;
 }
 
-static void __exit stm_driver_exit(void) {
-    i2c_unregister_device(stm_client);
-    i2c_del_driver(&stm_driver);
-    pr_info("Driver removed\n");
+static struct file_operations fops = {
+    .unlocked_ioctl = stm_ioctl
+};
+
+static int __init simple_drv_readtest_init(void) {
+    major_num = register_chrdev(STATIC_MAJOR, DEVICE_NAME, &fops);
+    if( major_num < 0 ) {
+        pr_err("Failed to register the device number\n");
+        return major_num;
+    } else if (major_num == 0) {
+        pr_info("Registered device with device number: %d\n", STATIC_MAJOR);
+        major_num = STATIC_MAJOR;
+    } else {
+        pr_info("Device %s registered with major: %d; minor: %d\n",
+                DEVICE_NAME, major_num >> 20, major_num & 0xFFFFF);
+    }
+
+    // Register class driver
+    char_class = class_create(CLASS_NAME);
+    if( IS_ERR(char_class)) {
+        unregister_chrdev(major_num, DEVICE_NAME);
+        return PTR_ERR(char_class);
+    }
+
+    pr_info("Class registered successfully; major#: %d\n", major_num);
+
+    // create the device file for the driver
+    dev_file = MKDEV(major_num, 0);
+    char_device = device_create(char_class, NULL, dev_file,
+                                NULL, DEVICE_NAME);
+    if( IS_ERR(char_device) ) {
+        class_destroy(char_class);
+        unregister_chrdev(major_num, DEVICE_NAME);
+        pr_info("Failed to create the device\n" );
+        return PTR_ERR(char_device);
+    }
+
+    pr_info("Success..!!!! Device driver created successfully\n");
+    return 0;
 }
 
-module_init(stm_driver_init);
-module_exit(stm_driver_exit);
+static void __exit simple_drv_readtest_exit(void) {
+    device_destroy(char_class, dev_file);
+    class_unregister(char_class);
+    class_destroy(char_class);
+    unregister_chrdev(major_num, DEVICE_NAME);
+    pr_info("Bye kernel\n");
+	return;
+}
 
+module_init(simple_drv_readtest_init);
+module_exit(simple_drv_readtest_exit);
 
+MODULE_DESCRIPTION("This is a simple I2C driver to blink "
+                   "the built-in LED of STM32F103C6T6A");
+MODULE_AUTHOR("Mohammed Shafeeque");
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Muhammed Shafeeque K N");
-MODULE_DESCRIPTION("A simple I2C driver to interface STM32F103C6T6 board");
-MODULE_VERSION("1.0");
